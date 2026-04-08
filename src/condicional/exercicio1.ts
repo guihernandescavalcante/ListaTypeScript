0cfb9b84100adc10e8bcd9c0152862629b40b9d0
wendy-dev-byte <mileachristibarbosa@gmail.com>
2026-04-08 10:38:29 -0300

    1
    atividade

---
+++ src/condicional/exercicio1.ts
@@ -0,0 +1,9 @@
+let numero:number = Number(prompt("Digite um numero: "))
+if(numero % 2 == 0){
+    console.log("Número é par")
+}else{
+    console.log("Número é impar")
+}
+if(numero > 0){
+    console.log("")
+}
